dbb46775c9cea331cc16a20b8d0962bb59051ddb
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-21 22:21:34 +0900

    feat: 금액 입력 가져오기 구현
    - 금액 입력
    - 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import InputView from './view/InputView.js';
+
+import tryInput from './util/tryInput.js';
+import validateAmount from './util/validator/amount.js';
 class App {
-  async run() {}
+  async run() {
+    const amount = await tryInput(() => this.getAmount());
+  }
+
+  async getAmount() {
+    const amount = await InputView.readAmount();
+
+    validateAmount(amount);
+
+    return amount;
+  }
 }
 
 export default App;
