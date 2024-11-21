1a1b7bd49707694f36527266317da246a0cf08dd
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-21 22:29:42 +0900

    feat: 로또 구매 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import LottoMachine from './model/LottoMachine.js';
 import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
 
 import tryInput from './util/tryInput.js';
 import validateAmount from './util/validator/amount.js';
 class App {
+  #lottoMachine;
+
+  constructor() {
+    this.#lottoMachine = new LottoMachine();
+  }
+
   async run() {
     const amount = await tryInput(() => this.getAmount());
+    const lottos = this.buyLottos(amount);
+
+    OutputView.printLottos(lottos);
   }
 
   async getAmount() {
@@ -13,6 +24,12 @@ class App {
     validateAmount(amount);
 
     return amount;
+  }
+
+  buyLottos(amount) {
+    this.#lottoMachine.generateLottos(amount);
+
+    return this.#lottoMachine.getLottos();
   }
 }
 
