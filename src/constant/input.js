f82f6b48809442b57c81c87459bf3c1601cefaae
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-21 22:18:59 +0900

    feat: 입력 메시지 상수로 정의

---
+++ src/constant/input.js
@@ -0,0 +1,7 @@
+const INPUT_MESSAGE = Object.freeze({
+  amount: '구입금액을 입력해 주세요.\n',
+  winningNumbers: '\n당첨 번호를 입력해 주세요.\n',
+  bonusNumber: '\n보너스 번호를 입력해 주세요.\n',
+});
+
+export default INPUT_MESSAGE;
